384503e6997c04e56332ed9aec428491b30173f9
alexissfry <alexissfry@gmail.com>
2023-09-07 11:04:03 -0400

    fix: mission statement

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -6,28 +6,25 @@ const Mission = () => {
     <div className="content-container">
       <h1 className="section-header">Mission</h1>
       <div className="text-container">
-        Women in Computer Science (WiCS) was formed by female undergraduate
-        students at Brown in the late 1980s to increase the participation of
-        women in the field of Computer Science. <br></br>
+        <strong>Women in Computer Science (WiCS)</strong> was formed by female
+        undergraduate students at Brown in the late 1980s to increase the
+        participation of women in the field of Computer Science. <br></br>
         <br />
         Affiliated with the Brown University Department of Computer Science,
         WiCS seeks to improve diversity and inclusion for underrepresented
         gender minorities in the technology industry through our{" "}
-        <span class="purple">
-          {" "}
-          social, mentoring, and outreach programs
-        </span>. <br></br>
+        <span class="purple"> mentoring and outreach programs </span> to connect
+        students and the greater Providence community with upperclassmen,
+        alumnae, and professors.<br></br>
         <br />
-        One highlight of WiCS is its organization of{" "}
-        <span class="purple"> social events</span> for female students within
-        the Computer Science department to meet and get to know each other, like
-        our scavenger hunt. We also enjoy partnering with other tech
-        organizations on campus, like MOSAIC+ and Hack@Brown, to{" "}
+        One highlight of WiCS is its organization of social events for students
+        within the Computer Science department to meet and get to know each
+        other, such as gingerbread house competitions and scavenger hunts. We
+        also enjoy partnering with other tech organizations on campus, like CS
+        DUG, Mosaic+, and Hack@Brown, to{" "}
         <span class="purple">build bridges across Brown's tech community</span>.
         Lastly, we love to host events with industry sponsors to connect our
         members with opportunities beyond the classroom.
-        <br></br>
-        <br></br>
       </div>
     </div>
   );
